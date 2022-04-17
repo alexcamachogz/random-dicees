e72c39aaad0dfde1d31b4a0a39db0ff2b20a3caf
Alex Camacho <camacho.ale94@gmai.com>
2022-04-17 10:37:11 -0500

    feature: changed programmatically the values of each dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    IBOulet allows me to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+//    Variables
+    let diceOptions = [ #imageLiteral(resourceName: "1") , #imageLiteral(resourceName: "2") , #imageLiteral(resourceName: "3") , #imageLiteral(resourceName: "4") , #imageLiteral(resourceName: "5") , #imageLiteral(resourceName: "6") ]
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        When the app load, the diceImageView1 should change
+//        diceImageView1.image = UIImage(named: "6")
+//        diceImageView1.alpha = 0.5
+//        diceImageView2.image = #imageLiteral(resourceName: "4")
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceOptions[leftDiceNumber]
+        diceImageView2.image = diceOptions[rightDiceNumber]
+        
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
 }
 
